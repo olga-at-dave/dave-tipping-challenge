7a55eb1cc91c2681268447a4597fd53c54232ca0
Adam Seiford <adam@dave.com>
2020-04-09 01:50:06 -0700

    ho man talk about flashbacks

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  advanceAmount = 75;
+  selectedBox = 1;
+  tipOptions = [
+    {
+      amount: 5,
+      trees: 5
+    },
+    {
+      amount: 10,
+      trees: 10
+    },
+    {
+      amount: 15,
+      trees: 30
+    },
+    {
+      amount: 20,
+      trees: 40
+    }
+  ];
+
+  onSelectOption(option: number) {
+    this.selectedBox = option;
+  }
+
+  onSubmit() {
+    alert('Thank you for your tip!');
+  }
 }
